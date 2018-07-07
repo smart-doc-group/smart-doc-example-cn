347db9b88eb773c8e61764e7cfa55d6826e84e91
oppofind <836575280@qq.com>
2018-07-07 11:03:03 +0800

    修改BaseController

--- src/main/java/com/power/doc/controller/BaseController.java
+++ src/main/java/com/power/doc/controller/BaseController.java
@@ -1,7 +1,6 @@
 package com.power.doc.controller;
 
 import com.power.common.model.CommonResult;
-import com.power.doc.annotation.ErrorCode;
 import com.power.doc.enums.ErrorCodeEnum;
 import com.power.doc.util.ResultUtil;
 import org.slf4j.Logger;
@@ -20,24 +19,53 @@ public abstract class BaseController {
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
 
     /**
-     * 自定义参数验证
+     * validate params
+     *
      * @param bindingResult
      * @return
      */
     protected CommonResult validParams(BindingResult bindingResult){
         if (bindingResult.hasErrors()){
             FieldError fieldError = bindingResult.getFieldError();
-            Class clazz = bindingResult.getTarget().getClass();
-            ErrorCode errorCode;
-            try{
-                errorCode = clazz.getDeclaredField(fieldError.getField()).getAnnotation(ErrorCode.class);
-            }catch (NoSuchFieldException e){
-                LOGGER.error("相关属性名不存在:{}",e);
-                return ResultUtil.returnError(ErrorCodeEnum.UNKNOWN_ERROR);
-            }
-            LOGGER.error("errorCode:{}",errorCode.value());
-            return ResultUtil.returnError(errorCode.value().getCode(),fieldError.getDefaultMessage());
+            return processBindingError(fieldError);
         }
         return ResultUtil.returnSuccess("");
+    }
+
+    /**
+     * 根据spring binding 错误信息自定义返回错误码和错误信息
+     *
+     * @param fieldError
+     * @return
+     */
+    private CommonResult processBindingError(FieldError fieldError){
+        String code = fieldError.getCode();
+        LOGGER.debug("validator error code:{}", code);
+        switch (code) {
+            case "NotEmpty":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_EMPTY.getCode(), fieldError.getDefaultMessage());
+            case "NotBlank":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_EMPTY.getCode(), fieldError.getDefaultMessage());
+            case "Pattern":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Min":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Max":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "DecimalMin":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "DecimalMax":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Size":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Digits":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Past":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            case "Future":
+                return ResultUtil.returnError(ErrorCodeEnum.PARAM_ERROR.getCode(), fieldError.getDefaultMessage());
+            default:
+                return ResultUtil.returnError(ErrorCodeEnum.UNKNOWN_ERROR);
+        }
     }
 }
