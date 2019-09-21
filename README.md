549c168212a302c12ee1995e1b2d0bc41f80ce47
oppofind <836575280@qq.com>
2019-09-22 00:29:28 +0800

    预制smart-doc 1.7版本功能

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ApplicationPower smart-doc接口文档自动生成工具测试代码。
 
 **注：** 本项目基本框架代码由application-power自动生成。想要体验者欢迎到https://github.com/shalousun/ApplicationPower
 或者https://gitee.com/sunyurepository/ApplicationPower浏览详情。
+
+# 访问api文档
+可以通过启动本项目，通过访问`http://localhost:8080/doc/api.html`来常看
+smart-doc根据源码生成的静态html文档。本功能待1.7版本发布后使用。
--- src/test/java/com/power/doc/service/ApiDocTest.java
+++ src/test/java/com/power/doc/service/ApiDocTest.java
@@ -18,6 +18,16 @@ import java.util.List;
 public class ApiDocTest {
 
     /**
+     * aes key
+     */
+    private static final String KEY = "aespower98765432";
+
+    /**
+     * 非ECB加密模式的向量
+     */
+    private static final String IV = "7201084316056726";
+
+    /**
      * 简单型接口，不需要指定请求头，并且项目是maven的.
      */
     @Test
@@ -36,6 +46,9 @@ public class ApiDocTest {
         config.setServerUrl("http://localhost:8080");
         config.setStrict(true);//true会严格要求注释，推荐设置true
         //config.setAllInOne(true);//true会将文档合并导出到一个markdown
+
+        //指定文档输出路径
+        //@since 1.7 版本开始，选择生成静态html doc文档可使用该路径：DocGlobalConstants.HTML_DOC_OUT_PATH;
         config.setOutPath("d:\\md");
         // @since 1.2,如果不配置该选项，则默认匹配全部的controller,
         // 如果需要配置有多个controller可以使用逗号隔开
@@ -50,7 +63,8 @@ public class ApiDocTest {
 
         //设置请求头，如果没有请求头，可以不用设置
         config.setRequestHeaders(
-                ApiReqHeader.header().setName("access_token").setType("string").setDesc("Basic auth credentials"),
+                ApiReqHeader.header().setName("access_token").setType("string")
+                        .setDesc("Basic auth credentials").setRequired(true).setSince("v1.1.0"),
                 ApiReqHeader.header().setName("user_uuid").setType("string").setDesc("User Uuid key")
         );
         //对于外部jar的类，编译后注释会被擦除，无法获取注释，但是如果量比较多请使用setSourcePaths来加载外部代码
@@ -78,10 +92,18 @@ public class ApiDocTest {
                 RevisionLog.getLog().setRevisionTime("2018/12/16").setAuthor("chen2").setRemarks("测试2").setStatus("修改").setVersion("V2.0")
         );
 
+        //since 1.7+版本开始，由于支持生成html的静态文档，为了防止controller名字直接暴露，
+        //因此smart-doc在生成文件名时采用用aes算法根据controller名称生成html的文件名，生成的注意文件名并不能反解密
+//        config.setAesInfo(
+//                ApiAesInfo.create().setKey(KEY).setVector(IV)
+//        );
+
         long start = System.currentTimeMillis();
         ApiDocBuilder.builderControllersApi(config);
+
+        //@since 1.7+版本开始，smart-doc支撑生成类似gitbook样式html文档，html文档可选择下面额方式
+        //HtmlApiDocBuilder.builderControllersApi(config);
         long end = System.currentTimeMillis();
         DateTimeUtil.printRunTime(end, start);
     }
-
 }
