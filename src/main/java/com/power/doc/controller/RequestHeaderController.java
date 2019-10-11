df715aa6af004296e80b501b2e30f05fd9f95555
xingzi <1771761586@qq.com>
2019-10-11 22:40:33 +0800

    增加@RequestHeader注解的支持

---
+++ src/main/java/com/power/doc/controller/RequestHeaderController.java
@@ -0,0 +1,14 @@
+package com.power.doc.controller;
+
+import org.springframework.web.bind.annotation.*;
+/**
+ * @author xingzi
+ * @date 2019 10 11  22:35
+ */
+@RestController
+public class RequestHeaderController {
+    @GetMapping("testRequstHeader")
+    public void testNormalParam(@RequestHeader(value = "token",defaultValue = "da" ,required = false)String name, Integer age) {
+
+    }
+}
