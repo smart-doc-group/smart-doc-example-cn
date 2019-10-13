e5999a91b269302a69eabd0263926038d26aaa34
oppofind <836575280@qq.com>
2019-10-13 23:57:35 +0800

    添加RequestHeader支持

--- src/main/java/com/power/doc/controller/RequestHeaderController.java
+++ src/main/java/com/power/doc/controller/RequestHeaderController.java
@@ -2,13 +2,20 @@ package com.power.doc.controller;
 
 import org.springframework.web.bind.annotation.*;
 /**
+ * RequestHeader注解测试
  * @author xingzi
  * @date 2019 10 11  22:35
  */
 @RestController
 public class RequestHeaderController {
-    @GetMapping("testRequstHeader")
-    public void testRequstHeader(@RequestHeader(value = "token",defaultValue = "da" ,required = false)String name, Integer age) {
+
+    /**
+     * 测试RequestHeader
+     * @param name 请求头(name)
+     * @param age   年龄
+     */
+    @GetMapping("testRequestHeader")
+    public void testRequestHeader(@RequestHeader(value = "token",defaultValue = "da" ,required = false)String name, Integer age) {
 
     }
 }
--- src/test/java/com/power/doc/service/ApiDocTest.java
+++ src/test/java/com/power/doc/service/ApiDocTest.java
@@ -2,11 +2,13 @@ package com.power.doc.service;
 
 import com.power.common.util.DateTimeUtil;
 import com.power.doc.builder.ApiDocBuilder;
+import com.power.doc.builder.HtmlApiDocBuilder;
 import com.power.doc.constants.DocGlobalConstants;
 import com.power.doc.enums.ErrorCodeEnum;
 import com.power.doc.model.*;
 import org.junit.Test;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,53 +36,16 @@ public class ApiDocTest {
 
         //指定文档输出路径
         //@since 1.7 版本开始，选择生成静态html doc文档可使用该路径：DocGlobalConstants.HTML_DOC_OUT_PATH;
-        config.setOutPath("d:\\md");
+        config.setOutPath(DocGlobalConstants.HTML_DOC_OUT_PATH);
         // @since 1.2,如果不配置该选项，则默认匹配全部的controller,
         // 如果需要配置有多个controller可以使用逗号隔开
-        config.setPackageFilters("com.power.doc.controller");
-        //不指定SourcePaths默认加载代码为项目src/main/java下的,如果项目的某一些实体来自外部代码可以一起加载
-        config.setSourceCodePaths(
-                //自1.7.0版本开始，在此处可以不设置本地代码路径，单独添加外部代码路径即可
-//            SourceCodePath.path().setDesc("本项目代码").setPath("src/main/java"),
-            SourceCodePath.path().setDesc("加载项目外代码").setPath("E:\\ApplicationPower\\ApplicationPower\\Common-util\\src\\main\\java")
-        );
-
-        //设置请求头，如果没有请求头，可以不用设置
-        config.setRequestHeaders(
-                ApiReqHeader.header().setName("access_token").setType("string").setDesc("Basic auth credentials"),
-                ApiReqHeader.header().setName("user_uuid").setType("string").setDesc("User Uuid key")
-        );
-        //对于外部jar的类，编译后注释会被擦除，无法获取注释，但是如果量比较多请使用setSourcePaths来加载外部代码
-        //如果有这种场景，则自己添加字段和注释，api-doc后期遇到同名字段则直接给相应字段加注释
-        config.setCustomResponseFields(
-                CustomRespField.field().setName("success").setDesc("成功返回true,失败返回false"),
-                CustomRespField.field().setName("message").setDesc("接口响应信息"),
-                CustomRespField.field().setName("data").setDesc("接口响应数据"),
-                CustomRespField.field().setName("code").setValue("00000").setDesc("响应代码")
-        );
-
-        //设置项目错误码列表，设置自动生成错误列表,
-        List<ApiErrorCode> errorCodeList = new ArrayList<>();
-        for (ErrorCodeEnum codeEnum : ErrorCodeEnum.values()) {
-            ApiErrorCode errorCode = new ApiErrorCode();
-            errorCode.setValue(codeEnum.getCode()).setDesc(codeEnum.getDesc());
-            errorCodeList.add(errorCode);
-        }
-        //如果没需要可以不设置
-        config.setErrorCodes(errorCodeList);
-
-        //非必须只有当setAllInOne设置为true时文档变更记录才生效，https://gitee.com/sunyurepository/ApplicationPower/issues/IPS4O
-        config.setRevisionLogs(
-                RevisionLog.getLog().setRevisionTime("2018/12/15").setAuthor("chen").setRemarks("测试").setStatus("创建").setVersion("V1.0"),
-                RevisionLog.getLog().setRevisionTime("2018/12/16").setAuthor("chen2").setRemarks("测试2").setStatus("修改").setVersion("V2.0")
-        );
+//        config.setPackageFilters("com.power.doc.controller");
 
 
         long start = System.currentTimeMillis();
-        ApiDocBuilder.builderControllersApi(config);
-
-        //@since 1.7+版本开始，smart-doc支撑生成类似gitbook样式html文档，html文档可选择下面额方式
-        //HtmlApiDocBuilder.builderControllersApi(config);
+        //获取接口数据后自行处理
+        HtmlApiDocBuilder.builderControllersApi(config);
+        //List<ApiDoc> docList = ApiDocBuilder.listOfApiData(config);
         long end = System.currentTimeMillis();
         DateTimeUtil.printRunTime(end, start);
     }
