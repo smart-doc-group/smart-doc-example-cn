ef52d339c7cef4e6e458213669c0ea888b94a82b
oppofind <836575280@qq.com>
2019-11-06 11:01:18 +0800

    添加订单状态

---
+++ src/main/java/com/power/doc/enums/OrderEnum.java
@@ -0,0 +1,32 @@
+package com.power.doc.enums;
+
+/**
+ * 订单状态
+ * @author yu 2019/11/5.
+ */
+public enum OrderEnum {
+
+    WAIT_PAY("0", "已支付"),
+
+    PAID("1", "已支付"),
+
+    EXPIRED("2","已经失效");
+
+    private String code;
+
+    private String desc;
+
+    OrderEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+
+    public String getDesc() {
+        return this.desc;
+    }
+}
