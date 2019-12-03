37403e910f69bf6d54ec70caa1ea2638c7c59433
oppofind <836575280@qq.com>
2019-12-04 00:57:16 +0800

    提交新测试

--- src/main/java/com/power/doc/controller/ApiNoteController.java
+++ src/main/java/com/power/doc/controller/ApiNoteController.java
@@ -1,7 +1,12 @@
 package com.power.doc.controller;
 
+import com.power.doc.entity.KeyValuePair;
+import com.power.doc.entity.SimpleUser;
+import com.power.doc.entity.XbootBaseEntity;
 import com.power.doc.model.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * apiNote注解测试
@@ -16,7 +21,17 @@ public class ApiNoteController {
      * @return
      */
     @PostMapping(value = "/test")
-    public String test(@RequestBody User user){
+    public String test(@RequestBody List<SimpleUser> user){
         return "hello apiNote";
+    }
+
+    /**
+     * 测试key value对泛型
+     * @param keyValuePair
+     * @return
+     */
+    @PostMapping(value = "testKeyValue")
+    public String testKV(@RequestBody KeyValuePair<SimpleUser,XbootBaseEntity> keyValuePair) {
+        return "";
     }
 }
