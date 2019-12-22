4ac0c5ed378abd12d24c108e0a512aefb430abfe
oppofind <836575280@qq.com>
2019-12-23 00:44:04 +0800

    修改formdata测试用例

--- src/main/java/com/power/doc/controller/FormDataController.java
+++ src/main/java/com/power/doc/controller/FormDataController.java
@@ -2,6 +2,8 @@ package com.power.doc.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.power.doc.entity.SimpleUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 接收表单参数
  * @author yu 2019/10/29.
  */
+@Slf4j
 @RestController
 public class FormDataController {
 
@@ -19,6 +22,26 @@ public class FormDataController {
      */
     @PostMapping("formData1")
     public void test(SimpleUser simpleUser){
-        System.out.println(JSON.toJSONString(simpleUser));
+       log.info(JSON.toJSONString(simpleUser));
     }
+
+    /**
+     * 测试formData带路径参数
+     * @param simpleUser
+     */
+    @PostMapping("formData1/{id}")
+    public void test(@PathVariable String id, SimpleUser simpleUser){
+        log.info(JSON.toJSONString(simpleUser));
+    }
+
+    /**
+     * 测试formData带路多个路径参数
+     * @param simpleUser
+     */
+    @PostMapping("formData1/{id}/{age}")
+    public void test(@PathVariable String id,@PathVariable Integer age, SimpleUser simpleUser){
+        log.info(JSON.toJSONString(simpleUser));
+    }
+
+
 }
