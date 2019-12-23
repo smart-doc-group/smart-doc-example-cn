b33d8061bf77b11f29b7fc69d7f0b20819d63759
xingzi <1771761586@qq.com>
2019-12-23 10:32:19 +0800

    增加formData测试

--- src/main/java/com/power/doc/controller/FormDataController.java
+++ src/main/java/com/power/doc/controller/FormDataController.java
@@ -2,9 +2,14 @@ package com.power.doc.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.power.doc.entity.SimpleUser;
+import com.power.doc.enums.SimpleEnum;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * 接收表单参数
@@ -20,5 +25,53 @@ public class FormDataController {
     @PostMapping("formData1")
     public void test(SimpleUser simpleUser){
         System.out.println(JSON.toJSONString(simpleUser));
+    }
+
+    /**
+     * Post请求发表单文件
+     */
+    @PostMapping("formData2")
+    public void formData2(String userId, String userData, MultipartFile file){
+        System.out.println("success");
+    }
+
+    /**
+     * Get请求发表单文件 暂不支持完全解析
+     */
+    @GetMapping("formData2")
+    public void formData3(String userId, String userData, MultipartFile file){
+        System.out.println("success");
+    }
+
+    /**
+     * 表单 包含复杂对象的Get 暂不支持完全解析
+     */
+    @GetMapping("formData4")
+    public void formData4(String userId, String userData, SimpleUser simpleUser){
+        System.out.println("success");
+    }
+    /**
+     * 表单 包含复杂对象
+     */
+    @PostMapping("formData5")
+    public void formData5(String userId, String userData, SimpleUser simpleUser){
+        System.out.println("success");
+    }
+
+    /**
+     * 表单 枚举
+     */
+    @GetMapping("formData6")
+    public void formData6(String userId, String userData, SimpleEnum simpleUser){
+        System.out.println("success");
+    }
+
+
+    /**
+     *表单 ListObject>
+     */
+    @PostMapping("formData7")
+    public void formData7(String userId, String userData, List<SimpleUser> simpleUser){
+        System.out.println("success");
     }
 }
