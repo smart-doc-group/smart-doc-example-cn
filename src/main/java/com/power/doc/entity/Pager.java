fe521991040beaef4b05f829f751183766752425
oppofind <836575280@qq.com>
2019-12-25 09:30:15 +0800

    添加Pager

---
+++ src/main/java/com/power/doc/entity/Pager.java
@@ -0,0 +1,14 @@
+package com.power.doc.entity;
+
+import lombok.Data;
+
+/**
+ * @author yu 2019/12/18.
+ */
+@Data
+public class Pager {
+
+    private int concurrentPage;
+
+    private int pageSize;
+}
