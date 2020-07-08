e7ff18425ac5ee2fca614ae2471516beadd05f5f
xingzi <1771761586@qq.com>
2020-07-08 10:31:21 +0800

    修改插件和smartdoc版本 禁止自动装配数据库

--- src/main/java/com/power/doc/SpringBootMainApplication.java
+++ src/main/java/com/power/doc/SpringBootMainApplication.java
@@ -2,11 +2,12 @@ package com.power.doc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author yu on 2018/06/27.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMainApplication.class, args);
