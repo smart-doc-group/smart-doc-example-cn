1c3d12a2fccfd859e8fc6d42e7a850612d4995f5
oppofind <836575280@qq.com>
2020-10-17 22:28:11 +0800

    修改测试

--- src/main/java/com/power/doc/dubbo/DubboInterface.java
+++ src/main/java/com/power/doc/dubbo/DubboInterface.java
@@ -2,13 +2,19 @@ package com.power.doc.dubbo;
 
 import com.github.pagehelper.PageInfo;
 import com.power.common.model.CommonResult;
+import com.power.doc.entity.Order;
 import com.power.doc.model.UserDetail;
 
 /**
+ * dubbo接口测试
  * @dubbo
  * @author yu 2020/7/29.
  */
 public interface DubboInterface {
 
-    public CommonResult<PageInfo<UserDetail>> testConstantsRequestParams();
+    /**
+     * 测试
+     * @return
+     */
+    CommonResult<? extends Order> testConstantsRequestParams();
 }
