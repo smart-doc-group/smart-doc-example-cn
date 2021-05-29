5d2f580eef3081f55cfad58586da7733a15c4430
shalousun <smartdoc647@gmail.com>
2021-05-29 11:43:31 +0800

    添加上传

--- src/main/java/com/power/doc/controller/BaseController.java
+++ src/main/java/com/power/doc/controller/BaseController.java
@@ -1,12 +1,14 @@
 package com.power.doc.controller;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * BaseController
@@ -17,6 +19,15 @@ public abstract class BaseController<T> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
 
+    /**
+     * excel文件
+     */
+    public static final String EXCEL_CONTENT_TYPE = "application/vnd.ms-excel;charset=utf-8";
+
+    /**
+     * 普通的文本
+     */
+    public static final String TEXT_CONTENT_TYPE = "application/octet-stream;charset=utf-8";
 
     /**
      * 分页查询
@@ -31,5 +42,47 @@ public abstract class BaseController<T> {
     public T getPageList(@RequestBody T t) {
 
         return null;
+    }
+
+
+
+
+    /**
+     * 导出excel,添加文件名时需要自己添加后缀
+     *
+     * @param fileName 文件名(用户信息表.xls)
+     * @param response HttpServletResponse
+     * @return ServletOutputStream
+     * @throws Exception
+     */
+    protected ServletOutputStream exportExcel(String fileName, HttpServletResponse response) throws IOException {
+        return baseDownload(EXCEL_CONTENT_TYPE,fileName,response);
+    }
+
+    /**
+     * 基础的文件下载
+     * @param contentType 下载文件的类型
+     * @param fileName
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    protected ServletOutputStream baseDownload(String contentType, String fileName, HttpServletResponse response)
+            throws IOException {
+        response.setContentType(contentType);
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename="
+                + new String(fileName.getBytes("gbk"), "iso-8859-1"));
+        return response.getOutputStream();
+    }
+
+    /**
+     * 文件下载
+     * @param fileName 下载文件
+     * @param response 响应
+     * @return
+     * @throws IOException
+     */
+    protected ServletOutputStream downloadText(String fileName,HttpServletResponse response) throws IOException{
+        return baseDownload(TEXT_CONTENT_TYPE,fileName,response);
     }
 }
---
+++ src/main/java/com/power/doc/controller/FileDownloadController.java
@@ -0,0 +1,73 @@
+package com.power.doc.controller;
+
+
+import com.power.common.util.RandomUtil;
+import com.power.common.util.StringUtil;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author yu 2021/5/29.
+ */
+@RestController
+public class FileDownloadController extends BaseController{
+
+
+    /**
+     * 下载文件
+     * @param filename 文件名|me
+     * @return
+     */
+    @PostMapping("download1/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        String fileName = filename+".log";
+        String randomStr = RandomUtil.randomNumbers(50);
+        Resource resource = new ByteArrayResource(randomStr.getBytes());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    /**
+     * 下载普通文件文件
+     *
+     * @param response
+     * @return
+     * @throws IOException
+     * @download
+     */
+    @PostMapping("text/")
+    public void download(HttpServletResponse response) throws IOException {
+        String randomStr = RandomUtil.randomNumbers(50);
+        String fileName = "test.log";
+        //要使用smart-doc debug页面测试文件下载，则必须设置filename响应头，否则请采用其他模拟工具测试。
+        // urlDecode用于处理中文件名
+        response.setHeader("filename", urlEncode(fileName));// since 2.0.2后不需要这样设置
+        ServletOutputStream outputStream = this.downloadText(fileName, response);
+        outputStream.write(randomStr.getBytes());
+    }
+
+    public String urlEncode(String str) {
+        if (StringUtil.isEmpty(str)) {
+            return null;
+        } else {
+            try {
+                return java.net.URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+}
