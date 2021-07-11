1bfb8cdd1bbc16366aeb24b548f1474b7c8a2b75
shalousun <smartdoc647@gmail.com>
2021-07-11 21:15:07 +0800

    添加header常量测试

---
+++ src/main/java/com/power/doc/constants/HeaderConstants.java
@@ -0,0 +1,9 @@
+package com.power.doc.constants;
+
+/**
+ * @author yu 2021/7/11.
+ */
+public interface HeaderConstants {
+
+    String TOKEN = "token";
+}
--- src/main/java/com/power/doc/controller/RequestHeaderController.java
+++ src/main/java/com/power/doc/controller/RequestHeaderController.java
@@ -1,10 +1,15 @@
 package com.power.doc.controller;
 
+import com.power.doc.constants.HeaderConstants;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * RequestHeader注解测试
+ *
  * @author xingzi
  * @date 2019 10 11  22:35
  */
@@ -14,8 +19,9 @@ public class RequestHeaderController {
 
     /**
      * 测试RequestHeader常规使用
+     *
      * @param name 请求头(name)
-     * @param age   年龄
+     * @param age  年龄
      */
     @GetMapping("testRequestHeader")
     public void testRequestHeader(@RequestHeader String name, Integer age) {
@@ -24,29 +30,32 @@ public class RequestHeaderController {
 
     /**
      * 测试RequestHeader绑定参数名
+     *
      * @param name 请求头(name)
-     * @param age   年龄
+     * @param age  年龄
      */
     @GetMapping("testRequestHeader/value")
-    public void testRequestHeaderAnnotationWithValue(@RequestHeader(value = "token", required = false)String name,@RequestHeader Integer age) {
+    public void testRequestHeaderAnnotationWithValue(@RequestHeader(value = "token", required = false) String name, @RequestHeader Integer age) {
 
     }
 
     /**
      * 测试RequestHeader绑定默认值
+     *
      * @param name 请求头(name)
-     * @param age   年龄
+     * @param age  年龄
      */
     @GetMapping("testRequestHeader/DefaultVal")
-    public void testRequestHeaderAnnotationWithDefaultVal(@RequestHeader(value = "token",defaultValue = "da",required = false)String name, Integer age) {
+    public void testRequestHeaderAnnotationWithDefaultVal(@RequestHeader(value = "token", defaultValue = "da", required = false) String name, Integer age) {
 
     }
 
     /**
      * 测试Mapping中的headers属性
+     *
      * @return
      */
-    @GetMapping(value = "testRequestHeader/foos", headers = { "key1=val1", "key2=val2" })
+    @GetMapping(value = "testRequestHeader/foos", headers = {"key1=val1", "key2=val2"})
     @ResponseBody
     public String getFoosWithHeaders() {
         return "Get some Foos with Header";
@@ -54,12 +63,46 @@ public class RequestHeaderController {
 
     /**
      * 测试Mapping header属性
+     *
      * @return
      */
     @GetMapping(value = "/ex/foos", headers = "key=val")
     @ResponseBody
     public String getFoosWithHeader() {
         return "Get some Foos with Header";
+    }
+
+    /**
+     * 测试常量请求头1
+     *
+     * @param token 请求头
+     * @return
+     */
+    @GetMapping("testRequestHeader/constants1")
+    public String testConstants(@RequestHeader(HeaderConstants.TOKEN) String token) {
+        return "header constants";
+    }
+
+    /**
+     * 测试常量请求头2
+     *
+     * @param token 请求头
+     * @return
+     */
+    @GetMapping("testRequestHeader/constant2")
+    public String testConstants2(@RequestHeader(value = HeaderConstants.TOKEN) String token) {
+        return "header constants";
+    }
+
+    /**
+     * 测试常量请求头3
+     *
+     * @param token 请求头
+     * @return
+     */
+    @GetMapping("testRequestHeader/constant3")
+    public String testConstants3(@RequestHeader(name= HeaderConstants.TOKEN) String token) {
+        return "header constants";
     }
 
 }
