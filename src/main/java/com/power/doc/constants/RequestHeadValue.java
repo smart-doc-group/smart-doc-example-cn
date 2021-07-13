f22413800793143eb3ab005f5f55d3c16a22d687
shalousun <smartdoc647@gmail.com>
2021-07-13 21:34:56 +0800

    添加FeignClient

---
+++ src/main/java/com/power/doc/constants/RequestHeadValue.java
@@ -0,0 +1,20 @@
+package com.power.doc.constants;
+
+/**
+ * 测试常量
+ * @author yu 2021/7/13.
+ */
+public class RequestHeadValue {
+    /**
+     * 测试常量值
+     */
+    public static final String TEST_BIND_VALUE = "MAJOR";
+    /**
+     * 测试常量值
+     */
+    public static final String PREFIX = "/APP/";
+    /**
+     * 测试常量值
+     */
+    public static final String URL = "www.baidu.com";
+}
--- src/main/java/com/power/doc/controller/AFeignInterface.java
+++ src/main/java/com/power/doc/controller/AFeignInterface.java
@@ -1,16 +1,31 @@
 package com.power.doc.controller;
 
+import com.power.doc.constants.RequestHeadValue;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * feign测试
- *
+ * @restApi
  * @author yu 2020/6/21.
  */
-@RequestMapping("feign")
+@FeignClient(value = "SER0",path = RequestHeadValue.PREFIX + RequestHeadValue.URL)
 public interface AFeignInterface {
 
+    /**
+     * 解析路径
+     * @param id 标识
+     * @param name 姓名
+     * @return un
+     */
+    @GetMapping(value = "/{id}/{name}")
+    String pathResolve(@PathVariable String id,@PathVariable String name);
+
+    /**
+     * 模具
+     * @return
+     */
     @GetMapping(value = "/enum")
     String test();
 }
