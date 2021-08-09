d40ea1e57f043d7a22fee532176fafd72ed86055
shalousun <smartdoc647@gmail.com>
2021-08-09 10:48:43 +0800

    生成新的文档

--- src/main/resources/static/doc/search.js
+++ src/main/resources/static/doc/search.js
@@ -23,7 +23,7 @@ api.push({
 })
 api[1].list.push({
     order: '1',
-    desc: 'Test Constants',
+    desc: 'TestConstants',
 });
 api.push({
     alias: 'bf96ff3caa364a37a099b5080ddcfa8e',
@@ -38,7 +38,7 @@ api[2].list.push({
 });
 api[2].list.push({
     order: '2',
-    desc: 'Test Constants',
+    desc: 'TestConstants',
 });
 api[2].list.push({
     order: '3',
@@ -59,13 +59,13 @@ api[2].list.push({
 api.push({
     alias: '2d2bdadc0ad46c43254dd04b8ca0382e',
     order: '4',
-    link: '*_部门操作_*_*_@author_jdkleo_*_@since_1.0',
-    desc: '* 部门操作 * * @author jdkleo * @since 1.0',
+    link: '*_部门操作 _* _*_@author_jdkleo _*_@since_1.0',
+    desc: '*部门操作 * *@authorjdkleo *@since1.0',
     list: []
 })
 api[3].list.push({
     order: '1',
-    desc: '* 获得部门     * @param deptId 部门ID     * @return',
+    desc: '*获得部门 *@paramdeptId部门ID *@return',
 });
 api.push({
     alias: '2e31411fe8d89320089936c2f1efc9f0',
@@ -106,668 +106,691 @@ api[5].list.push({
     desc: '循环依赖参数推导',
 });
 api.push({
-    alias: 'b200eb21a90c93ca3ff45e88a27f511e',
+    alias: 'b891f0bb0ee2077e20192caa07608b38',
     order: '7',
-    link: '枚举参数测试',
-    desc: '枚举参数测试',
+    link: 'json文件配置全局参数测试',
+    desc: 'json文件配置全局参数测试',
     list: []
 })
 api[6].list.push({
     order: '1',
-    desc: '获取枚举参数1',
+    desc: 'get请求测试query参数',
 });
 api[6].list.push({
     order: '2',
-    desc: '获取枚举参数2',
+    desc: 'post请求测试query参数',
 });
 api[6].list.push({
     order: '3',
-    desc: '获取枚举参数3',
+    desc: 'get请求测试query参数和path参数',
 });
 api[6].list.push({
     order: '4',
+    desc: 'post请求测试query参数和path参数',
+});
+api.push({
+    alias: 'b200eb21a90c93ca3ff45e88a27f511e',
+    order: '8',
+    link: '枚举参数测试',
+    desc: '枚举参数测试',
+    list: []
+})
+api[7].list.push({
+    order: '1',
+    desc: '获取枚举参数1',
+});
+api[7].list.push({
+    order: '2',
+    desc: '获取枚举参数2',
+});
+api[7].list.push({
+    order: '3',
+    desc: '获取枚举参数3',
+});
+api[7].list.push({
+    order: '4',
     desc: '获取枚举参数4',
 });
-api[6].list.push({
+api[7].list.push({
     order: '5',
     desc: '枚举响应',
 });
 api.push({
     alias: '50b94a04e9e3d590af0475e4ff73f6d1',
-    order: '8',
+    order: '9',
     link: '文件下载',
     desc: '文件下载',
     list: []
 })
-api[7].list.push({
+api[8].list.push({
     order: '1',
     desc: '下载文件',
 });
-api[7].list.push({
+api[8].list.push({
     order: '2',
     desc: '下载普通文件文件',
 });
-api[7].list.push({
+api[8].list.push({
     order: '3',
     desc: '分页查询',
 });
 api.push({
     alias: '83a3c6033a7056d66018d3aa768411c0',
-    order: '9',
+    order: '10',
     link: '文件上传测试',
     desc: '文件上传测试',
     list: []
 })
-api[8].list.push({
+api[9].list.push({
     order: '1',
     desc: '上传单个文件',
 });
-api[8].list.push({
+api[9].list.push({
     order: '2',
     desc: '批量上传文件',
 });
-api[8].list.push({
+api[9].list.push({
     order: '3',
     desc: '批量上传文件1',
 });
-api[8].list.push({
+api[9].list.push({
     order: '4',
     desc: '批量上传文件2',
 });
-api[8].list.push({
+api[9].list.push({
     order: '5',
     desc: '批量上传文件3',
 });
 api.push({
     alias: '2fa34eca8b82d94f10f2b13e05545202',
-    order: '10',
+    order: '11',
     link: '接收表单参数',
     desc: '接收表单参数',
     list: []
 })
-api[9].list.push({
+api[10].list.push({
     order: '1',
     desc: 'Post请求发表单文件',
 });
-api[9].list.push({
+api[10].list.push({
     order: '2',
-    desc: 'Get请求发表单文件 暂不支持完全解析',
+    desc: 'Get请求发表单文件暂不支持完全解析',
 });
-api[9].list.push({
+api[10].list.push({
     order: '3',
-    desc: '表单 包含复杂对象的Get 暂不支持完全解析',
+    desc: '表单包含复杂对象的Get暂不支持完全解析',
 });
-api[9].list.push({
+api[10].list.push({
     order: '4',
     desc: '测试formData带路径参数',
 });
-api[9].list.push({
+api[10].list.push({
     order: '5',
     desc: '测试formData带路多个路径参数',
 });
 api.push({
     alias: 'df9d31e103d016173443a8caa1420b0a',
-    order: '11',
+    order: '12',
     link: 'java泛型解析测试',
     desc: 'java泛型解析测试',
     list: []
 })
-api[10].list.push({
+api[11].list.push({
     order: '1',
     desc: '测试多泛型1',
 });
-api[10].list.push({
+api[11].list.push({
     order: '2',
     desc: '测试多泛型2',
 });
-api[10].list.push({
+api[11].list.push({
     order: '3',
     desc: '测试单泛型嵌套',
 });
 api.push({
     alias: 'd87b6b0ec113b406ac7c1e0db5af83ba',
-    order: '12',
+    order: '13',
     link: 'https测试',
     desc: 'https测试',
     list: []
 })
-api[11].list.push({
+api[12].list.push({
     order: '1',
     desc: '测试https',
 });
-api[11].list.push({
+api[12].list.push({
     order: '2',
     desc: '测试http',
 });
 api.push({
     alias: '535187f55e9e7260eef406de3747ee85',
-    order: '13',
+    order: '14',
     link: 'test_inner_class',
-    desc: 'Test inner class',
+    desc: 'Testinnerclass',
     list: []
 })
-api[12].list.push({
+api[13].list.push({
     order: '1',
-    desc: 'Return A object contains Inner class',
+    desc: 'ReturnAobjectcontainsInnerclass',
 });
 api.push({
     alias: '81cf0dd99f05b620fd425f2066899686',
-    order: '14',
+    order: '15',
     link: 'jdk8的时间测试',
     desc: 'JDK8的时间测试',
     list: []
 })
-api[13].list.push({
+api[14].list.push({
     order: '1',
     desc: 'LocalDate和LocalDateTime测试',
 });
 api.push({
     alias: '02a3a37ef782f51d4438de808ce09926',
-    order: '15',
+    order: '16',
     link: 'jpa代码测试',
     desc: 'Jpa代码测试',
     list: []
 })
-api[14].list.push({
+api[15].list.push({
     order: '1',
-    desc: 'Jpa Page',
+    desc: 'JpaPage',
 });
-api[14].list.push({
+api[15].list.push({
     order: '2',
-    desc: 'Jpa Pageable查询参数重渲染',
+    desc: 'JpaPageable查询参数重渲染',
 });
 api.push({
     alias: '1593d84f73104481afe38bd95469dee3',
-    order: '16',
+    order: '17',
     link: '测试json',
     desc: '测试json',
     list: []
 })
-api[15].list.push({
+api[16].list.push({
     order: '1',
     desc: 'RequestParam+json',
 });
 api.push({
     alias: '3b2eff453fb7e7009fdf3c39e921c8a0',
-    order: '17',
+    order: '18',
     link: 'fastjson和jackson注解支持测试',
     desc: 'FastJson和Jackson注解支持测试',
     list: []
 })
-api[16].list.push({
+api[17].list.push({
     order: '1',
     desc: 'Jackson注解支持测试',
 });
-api[16].list.push({
+api[17].list.push({
     order: '2',
     desc: 'FastJson注解支持测试',
 });
 api.push({
     alias: 'a12685ab343a6775a97efc4b7a03dce2',
-    order: '18',
+    order: '19',
     link: 'list返回接口api文档测试',
     desc: 'List返回接口Api文档测试',
     list: []
 })
-api[17].list.push({
+api[18].list.push({
     order: '1',
     desc: 'List&lt;String&gt;结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '2',
     desc: 'List&lt;Map&lt;String,String&gt;&gt;结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '3',
     desc: 'List&lt;自动义对象&gt;',
 });
-api[17].list.push({
+api[18].list.push({
     order: '4',
     desc: 'List&lt;Map&lt;String,T&gt;&gt;结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '5',
     desc: 'List&lt;Map&lt;M,N&lt;P,k&gt;&gt;&gt;超复杂结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '6',
     desc: 'List&lt;T&lt;List&lt;M&gt;,List&lt;M&gt;,List&lt;M&gt;&gt;&gt;超复杂结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '7',
     desc: 'List&lt;Teacher&lt;Teacher&lt;User,User,User&gt;,User,User&gt;&gt;结构',
 });
-api[17].list.push({
+api[18].list.push({
     order: '8',
     desc: 'List&lt;Teacher&lt;Teacher&lt;User,User,User&gt;,Teacher&lt;User,User,User&gt;,Teacher&lt;User,User,User&gt;&gt;&gt;',
 });
-api[17].list.push({
+api[18].list.push({
     order: '9',
     desc: 'CommonResult&lt;List&lt;UserDto&gt;&gt;',
 });
 api.push({
     alias: 'c4cb7d82dacdf712d5d3ff3d27f17403',
-    order: '19',
+    order: '20',
     link: 'map返回型接口api文档测试',
     desc: 'Map返回型接口api文档测试',
     list: []
 })
-api[18].list.push({
+api[19].list.push({
     order: '1',
     desc: 'Map&lt;String,Integer&gt;结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '2',
     desc: 'Map&lt;String,Object&gt;结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '3',
     desc: 'Map&lt;String,User&gt;结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '4',
     desc: 'Map&lt;String,Student&gt;结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '5',
     desc: 'Map&lt;String,Teacher&lt;List&lt;User&gt;,User,Student&gt;&gt;超复杂结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '6',
     desc: 'TreeMap&lt;String,Teacher&lt;List&lt;User&gt;,User,Student&gt;&gt;超复杂结构',
 });
-api[18].list.push({
+api[19].list.push({
     order: '7',
     desc: 'Map&lt;String,Teacher&lt;Map&lt;String,User&gt;,Map&lt;String,User&gt;,Map&lt;String,User&gt;&gt;&gt;超复杂结构',
 });
 api.push({
     alias: '0f4e6cf508bef37d9fa85a4b2730995c',
-    order: '20',
+    order: '21',
     link: '多泛型测试',
     desc: '多泛型测试',
     list: []
 })
-api[19].list.push({
+api[20].list.push({
     order: '1',
     desc: '数组',
 });
-api[19].list.push({
+api[20].list.push({
     order: '2',
-    desc: 'gitee #I1S8W验证',
+    desc: 'gitee#I1S8W验证',
 });
 api.push({
     alias: '3324a2d2160bb6482a38f1cb7bc07865',
-    order: '21',
+    order: '22',
     link: 'mybatis-plus分页测试',
     desc: 'mybatis-plus分页测试',
     list: []
 })
-api[20].list.push({
+api[21].list.push({
     order: '1',
     desc: 'mybatis-plus原始分页类测试',
 });
-api[20].list.push({
+api[21].list.push({
     order: '2',
     desc: 'mybatis-plus分页自定义处理测试',
 });
-api[20].list.push({
+api[21].list.push({
     order: '3',
     desc: 'mybatis-plus分页自定义处理测试2',
 });
 api.push({
     alias: '474eff721eee4c33056ab6a4c91b522b',
-    order: '22',
+    order: '23',
     link: '继承接口测试',
     desc: '继承接口测试',
     list: []
 })
-api[21].list.push({
+api[22].list.push({
     order: '1',
     desc: '继承接口测试',
 });
-api[21].list.push({
+api[22].list.push({
     order: '2',
     desc: '根据Id删除',
 });
-api[21].list.push({
+api[22].list.push({
     order: '3',
     desc: '根据Id更新',
 });
 api.push({
     alias: 'b973cbe1cadf337d94bfc498f7c80036',
-    order: '23',
+    order: '24',
     link: 'path参数测试',
     desc: 'Path参数测试',
     list: []
 })
-api[22].list.push({
+api[23].list.push({
     order: '1',
     desc: '接收数组类型pathVariable',
 });
-api[22].list.push({
+api[23].list.push({
     order: '2',
     desc: 'Path正则测试1',
 });
-api[22].list.push({
+api[23].list.push({
     order: '3',
     desc: 'Path正则测试2',
 });
-api[22].list.push({
+api[23].list.push({
     order: '4',
     desc: 'Path正则测试3',
 });
 api.push({
     alias: '3870c489a88b232f29acd03d65d30786',
-    order: '24',
+    order: '25',
     link: 'requestheader注解测试',
     desc: 'RequestHeader注解测试',
     list: []
 })
-api[23].list.push({
+api[24].list.push({
     order: '1',
     desc: '测试RequestHeader常规使用',
 });
-api[23].list.push({
+api[24].list.push({
     order: '2',
     desc: '测试RequestHeader绑定参数名',
 });
-api[23].list.push({
+api[24].list.push({
     order: '3',
     desc: '测试RequestHeader绑定默认值',
 });
-api[23].list.push({
+api[24].list.push({
     order: '4',
     desc: '测试Mapping中的headers属性',
 });
-api[23].list.push({
+api[24].list.push({
     order: '5',
-    desc: '测试Mapping header属性',
+    desc: '测试Mappingheader属性',
 });
-api[23].list.push({
+api[24].list.push({
     order: '6',
     desc: '测试常量请求头1',
 });
-api[23].list.push({
+api[24].list.push({
     order: '7',
     desc: '测试常量请求头2',
 });
-api[23].list.push({
+api[24].list.push({
     order: '8',
     desc: '测试常量请求头3',
 });
 api.push({
     alias: '74c3788ee40af9e4128f488b20ba0156',
-    order: '25',
+    order: '26',
     link: 'spring_boot_params_test',
-    desc: 'Spring boot params test',
+    desc: 'Springbootparamstest',
     list: []
 })
-api[24].list.push({
+api[25].list.push({
     order: '1',
-    desc: 'Test Normal param binding',
+    desc: 'TestNormalparambinding',
 });
-api[24].list.push({
+api[25].list.push({
     order: '2',
-    desc: 'Test @RequestBody User',
+    desc: 'Test@RequestBodyUser',
 });
-api[24].list.push({
+api[25].list.push({
     order: '3',
-    desc: 'Test @RequestBody Map',
+    desc: 'Test@RequestBodyMap',
 });
-api[24].list.push({
+api[25].list.push({
     order: '4',
-    desc: 'Test @RequestBody List',
+    desc: 'Test@RequestBodyList',
 });
-api[24].list.push({
+api[25].list.push({
     order: '5',
-    desc: 'Test Array Params',
+    desc: 'TestArrayParams',
 });
-api[24].list.push({
+api[25].list.push({
     order: '6',
-    desc: 'Test @PathVariable',
+    desc: 'Test@PathVariable',
 });
-api[24].list.push({
+api[25].list.push({
     order: '7',
-    desc: 'Test @RequestParam',
+    desc: 'Test@RequestParam',
 });
-api[24].list.push({
+api[25].list.push({
     order: '8',
-    desc: 'Test @RequestParam with value',
+    desc: 'Test@RequestParamwithvalue',
 });
-api[24].list.push({
+api[25].list.push({
     order: '9',
-    desc: 'Test @RequestParam with default value',
+    desc: 'Test@RequestParamwithdefaultvalue',
 });
-api[24].list.push({
+api[25].list.push({
     order: '10',
-    desc: 'Test much path',
+    desc: 'Testmuchpath',
 });
-api[24].list.push({
+api[25].list.push({
     order: '11',
-    desc: 'Test much path much parameter',
+    desc: 'Testmuchpathmuchparameter',
 });
-api[24].list.push({
+api[25].list.push({
     order: '12',
-    desc: 'Test Constants',
+    desc: 'TestConstants',
 });
-api[24].list.push({
+api[25].list.push({
     order: '13',
     desc: '测试mapping中的',
 });
-api[24].list.push({
+api[25].list.push({
     order: '14',
     desc: '测试mapping中的2',
 });
 api.push({
     alias: 'f340529f8762e58ffcb731de6eebb3bd',
-    order: '26',
+    order: '27',
     link: '请求类型测试',
     desc: '请求类型测试',
     list: []
 })
-api[25].list.push({
+api[26].list.push({
     order: '1',
-    desc: '测试GetMapping 无参数',
+    desc: '测试GetMapping无参数',
 });
-api[25].list.push({
+api[26].list.push({
     order: '2',
-    desc: '测试PostMapping 无参数',
+    desc: '测试PostMapping无参数',
 });
-api[25].list.push({
+api[26].list.push({
     order: '3',
-    desc: '测试PutMapping 无参数',
+    desc: '测试PutMapping无参数',
 });
-api[25].list.push({
+api[26].list.push({
     order: '4',
-    desc: 'DeleteMapping 无参数',
+    desc: 'DeleteMapping无参数',
 });
-api[25].list.push({
+api[26].list.push({
     order: '5',
     desc: '测试GetMapping',
 });
-api[25].list.push({
+api[26].list.push({
     order: '6',
-    desc: '测试PostMapping json',
+    desc: '测试PostMappingjson',
 });
-api[25].list.push({
+api[26].list.push({
     order: '7',
     desc: '测试PostMapping表单',
 });
-api[25].list.push({
+api[26].list.push({
     order: '8',
-    desc: '测试PutMapping json',
+    desc: '测试PutMappingjson',
 });
-api[25].list.push({
+api[26].list.push({
     order: '9',
     desc: '测试PutMapping表单',
 });
-api[25].list.push({
+api[26].list.push({
     order: '10',
     desc: '测试DeleteMapping',
 });
 api.push({
     alias: '96452f5f89d750b4b4e8e1ad77d0382f',
-    order: '27',
+    order: '28',
     link: 'test_responseentity',
-    desc: 'Test ResponseEntity',
+    desc: 'TestResponseEntity',
     list: []
 })
-api[26].list.push({
+api[27].list.push({
     order: '1',
-    desc: 'ResponseEntity return List',
+    desc: 'ResponseEntityreturnList',
 });
 api.push({
     alias: 'f7691266816d94978ac937de8fb8f9be',
-    order: '28',
+    order: '29',
     link: '普通java对象api文档测试',
     desc: '普通java对象api文档测试',
     list: []
 })
-api[27].list.push({
+api[28].list.push({
     order: '1',
     desc: '返回普通String测试',
 });
-api[27].list.push({
+api[28].list.push({
     order: '2',
     desc: '返回普通javabean',
 });
-api[27].list.push({
+api[28].list.push({
     order: '3',
     desc: '返回复杂实体数据',
 });
-api[27].list.push({
+api[28].list.push({
     order: '4',
     desc: 'Teacher&lt;Teacher&lt;User,User,User&gt;,Teacher&lt;User,User,User&gt;,Teacher&lt;User,User,User&gt;&gt;结构',
 });
-api[27].list.push({
+api[28].list.push({
     order: '5',
-    desc: 'Teacher&lt;List&lt;User&gt;, User, Student&gt;',
+    desc: 'Teacher&lt;List&lt;User&gt;,User,Student&gt;',
 });
-api[27].list.push({
+api[28].list.push({
     order: '6',
     desc: '测试SubUser',
 });
-api[27].list.push({
+api[28].list.push({
     order: '7',
     desc: '返回CommonResult&lt;SubUser&gt;',
 });
 api.push({
     alias: '0594295b034c35538e0760f86cca3de5',
-    order: '29',
+    order: '30',
     link: '简单对象测试2',
     desc: '简单对象测试2',
     list: []
 })
-api[28].list.push({
+api[29].list.push({
     order: '1',
     desc: 'CommonResult&lt;String&gt;',
 });
-api[28].list.push({
+api[29].list.push({
     order: '2',
     desc: '返回Staff&lt;Staff&lt;Staff&gt;&gt;',
 });
-api[28].list.push({
+api[29].list.push({
     order: '3',
     desc: '返回Staff&lt;String&gt;',
 });
-api[28].list.push({
+api[29].list.push({
     order: '4',
     desc: 'JAVA继承测试',
 });
 api.push({
     alias: 'e10ec235c550b692514fad1375ec0057',
-    order: '30',
+    order: '31',
     link: '注释tag用例',
     desc: '注释tag用例',
     list: []
 })
-api[29].list.push({
+api[30].list.push({
     order: '1',
-    desc: '测试apiNote tag',
+    desc: '测试apiNotetag',
 });
-api[29].list.push({
+api[30].list.push({
     order: '2',
-    desc: '自定义mock tag获取mock值',
+    desc: '自定义mocktag获取mock值',
 });
-api[29].list.push({
+api[30].list.push({
     order: '3',
-    desc: '测试@ignore tag',
+    desc: '测试@ignoretag',
 });
 api.push({
     alias: '5b46cd366e910ec668ca3bd765dfeab4',
-    order: '31',
+    order: '32',
     link: '用户信息操作接口',
     desc: '用户信息操作接口',
     list: []
 })
-api[30].list.push({
+api[31].list.push({
     order: '1',
     desc: '添加用户',
 });
-api[30].list.push({
+api[31].list.push({
     order: '2',
     desc: '更新用户',
 });
 api.push({
     alias: '79b79caffc058ab27cfc0e8b7357ac13',
-    order: '32',
+    order: '33',
     link: 'jsr303参数验证规范测试',
     desc: 'JSR303参数验证规范测试',
     list: []
 })
-api[31].list.push({
+api[32].list.push({
     order: '1',
     desc: '验证validate',
 });
-api[31].list.push({
+api[32].list.push({
     order: '2',
     desc: '验证通用参数',
 });
-api[31].list.push({
+api[32].list.push({
     order: '3',
     desc: '分组验证1',
 });
-api[31].list.push({
+api[32].list.push({
     order: '4',
     desc: '分组验证2',
 });
-api[31].list.push({
+api[32].list.push({
     order: '5',
     desc: '分组验证3',
 });
-api[31].list.push({
+api[32].list.push({
     order: '6',
     desc: '分页查询',
 });
 api.push({
     alias: 'b94a91460ec7059fa540fd68e26a5ee4',
-    order: '33',
+    order: '34',
     link: 'xss拦击测试',
     desc: 'xss拦击测试',
     list: []
 })
-api[32].list.push({
+api[33].list.push({
     order: '1',
     desc: 'xss过滤普通post请求',
 });
-api[32].list.push({
+api[33].list.push({
     order: '2',
     desc: 'xss过滤get请求',
 });
-api[32].list.push({
+api[33].list.push({
     order: '3',
     desc: 'xss过滤json数据',
 });
 api.push({
     alias: 'error',
-    order: '34',
+    order: '35',
     link: 'error_code_list',
     desc: '错误码列表',
     list: []
 })
 api.push({
     alias: 'dict',
-    order: '35',
+    order: '36',
     link: 'dict_list',
     desc: '数据字典',
     list: []
