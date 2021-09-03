14f9862375bd8f2d2b6fb310dddbedf117a89856
shalousun <smartdoc647@gmail.com>
2021-09-03 17:52:08 +0800

    Add test for FastMybatis

---
+++ src/main/java/com/power/doc/controller/FastMybatisController.java
@@ -0,0 +1,23 @@
+package com.power.doc.controller;
+
+import com.gitee.fastmybatis.core.query.param.PageParam;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * fastmybatis
+ */
+@RestController
+public class FastMybatisController {
+
+  /**
+   * fastmybatis测试
+   * @param param
+   */
+  @PostMapping("/fastmybatis")
+  public void test(@RequestBody PageParam param){
+
+  }
+}
