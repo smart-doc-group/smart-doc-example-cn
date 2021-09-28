f101b6ebea322d8abe6cc885a524b4c0daaacca2
shalousun <smartdoc647@gmail.com>
2021-09-28 19:55:11 +0800

    upgrade smart-doc to 2.2.7

--- src/test/java/com/power/doc/utils/JacksonUtilTest.java
+++ src/test/java/com/power/doc/utils/JacksonUtilTest.java
@@ -1,7 +1,12 @@
 package com.power.doc.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.power.doc.entity.RoleDTO;
 import com.power.doc.model.mybatisplus.MybatisPlusPage;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author yu 2021/7/11.
@@ -9,11 +14,24 @@ import com.power.doc.model.mybatisplus.MybatisPlusPage;
 
 public class JacksonUtilTest {
 
+    @Test
     public void testToJsonString(){
         MybatisPlusPage mybatisPlusPage = new MybatisPlusPage();
         mybatisPlusPage.setCurrent(1L);
         mybatisPlusPage.setSize(1L);
         mybatisPlusPage.setTotal(1L);
+
+        String url ="null";
+        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?");
+        Matcher matcher = pattern.matcher(url);
+        if(matcher.find()){
+            String protocol = matcher.group(1);
+            String domain   = matcher.group(2);
+            String port     = matcher.group(3);
+            String uri      = matcher.group(4);
+        }
+
+
     }
 
 }
