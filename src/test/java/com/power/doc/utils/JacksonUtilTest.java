0cc1d2e4683a3e432b7f3cfcbbfa5fb8641a5162
shalousun <smartdoc647@gmail.com>
2021-10-07 11:03:12 +0800

    端口域名获取测试

--- src/test/java/com/power/doc/utils/JacksonUtilTest.java
+++ src/test/java/com/power/doc/utils/JacksonUtilTest.java
@@ -1,12 +1,10 @@
 package com.power.doc.utils;
 
+import com.power.doc.model.mybatisplus.MybatisPlusPage;
+import org.junit.jupiter.api.Test;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.power.doc.entity.RoleDTO;
-import com.power.doc.model.mybatisplus.MybatisPlusPage;
-
-import org.junit.jupiter.api.Test;
 
 /**
  * @author yu 2021/7/11.
@@ -15,20 +13,24 @@ import org.junit.jupiter.api.Test;
 public class JacksonUtilTest {
 
     @Test
-    public void testToJsonString(){
+    public void testToJsonString() {
         MybatisPlusPage mybatisPlusPage = new MybatisPlusPage();
         mybatisPlusPage.setCurrent(1L);
         mybatisPlusPage.setSize(1L);
         mybatisPlusPage.setTotal(1L);
 
-        String url ="null";
-        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?");
+        String url = "http://{{host}}:{{port}}/async-deferredresult";
+        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:[a-zA-Z-z0-9|^{{\\}}$]*)?(.*)?");
         Matcher matcher = pattern.matcher(url);
-        if(matcher.find()){
+        if (matcher.find()) {
             String protocol = matcher.group(1);
-            String domain   = matcher.group(2);
-            String port     = matcher.group(3);
-            String uri      = matcher.group(4);
+            String domain = matcher.group(2);
+            String port = matcher.group(3);
+            String uri = matcher.group(4);
+            System.out.println("protocol:" + protocol);
+            System.out.println("domain:" + domain);
+            System.out.println("port:" + port);
+            System.out.println("uri:" + uri);
         }
 
 
