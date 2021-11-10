b618cead7618418f99bdc3a9a0cf5e8aea61cc4b
shalousun <smartdoc647@gmail.com>
2021-11-10 19:38:48 +0800

    upgrade smart-doc to 2.3.0

---
+++ src/main/java/com/power/doc/controller/SessionSettingController.java
@@ -0,0 +1,28 @@
+package com.power.doc.controller;
+
+import com.power.common.model.CommonResult;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+/**
+ * 测试session参数忽略
+ *
+ * @author yu on 2021/11/10
+ */
+@RestController
+@RequestMapping("session")
+public class SessionSettingController {
+
+  /**
+   * session忽略
+   *
+   * @param userName 忽略用户名
+   */
+  @GetMapping("/attribute")
+  public CommonResult testSession(@SessionAttribute String userName) {
+    return CommonResult.ok();
+  }
+}
