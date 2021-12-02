2a6348484af7332f746b748315b3d591ad0b26f9
shalousun <smartdoc647@gmail.com>
2021-12-03 00:06:35 +0800

    升级smart-doc

--- src/main/java/com/power/doc/controller/JpaController.java
+++ src/main/java/com/power/doc/controller/JpaController.java
@@ -3,7 +3,6 @@ package com.power.doc.controller;
 import com.power.common.model.CommonResult;
 import com.power.doc.entity.RoleDTO;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,7 +31,7 @@ public class JpaController {
     /**
      * Jpa Pageable查询参数重渲染
      * @apiNote 测试让smart-doc使用自定义对象来渲染生成文档
-     * @param pageable com.power.doc.model.PageRequestDto
+     * @param pageable
      */
     @PostMapping(value = "/pageableParam")
     public CommonResult testRender(@RequestBody Pageable pageable) {
