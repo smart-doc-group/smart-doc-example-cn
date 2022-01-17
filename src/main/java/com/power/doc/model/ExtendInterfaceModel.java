f8d7d2e06c08000c7bb3b71b5448f95c6e9fe61f
shalousun <smartdoc647@gmail.com>
2022-01-17 23:46:26 +0800

    升级smart-doc

---
+++ src/main/java/com/power/doc/model/ExtendInterfaceModel.java
@@ -0,0 +1,17 @@
+package com.power.doc.model;
+
+/**
+ * @author yu 2021/12/17.
+ */
+public class ExtendInterfaceModel implements ModelInterface{
+
+    /**
+     * name
+     */
+    private String name;
+
+    @Override
+    public int getMoney() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/power/doc/model/ModelInterface.java
@@ -0,0 +1,10 @@
+package com.power.doc.model;
+
+/**
+ * @author yu 2021/12/17.
+ */
+public interface ModelInterface {
+    String TOKEN = "test";
+
+    int getMoney();
+}
