839a188830f2a1d5c26cfef97a5378871a9a5dca
chenqi <1535985458@qq.com>
2022-04-10 01:55:13 +0800

    test(feign): test feign client extends interface

--- src/main/java/com/power/doc/controller/feign/AFeignInterface.java
+++ src/main/java/com/power/doc/controller/feign/AFeignInterface.java
@@ -4,13 +4,14 @@ import com.power.doc.constants.RequestHeadValue;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * feign测试
  * @restApi
  * @author yu 2020/6/21.
  */
-@FeignClient(value = "SER0",path = RequestHeadValue.PREFIX + RequestHeadValue.URL)
+@RequestMapping("/fafaf")
 public interface AFeignInterface {
 
     /**
