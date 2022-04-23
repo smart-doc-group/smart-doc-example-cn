04e813ca4b1813c386bed621051398f7b720ce20
luchuanbaker <luchuanbaker@qq.com>
2022-04-23 15:02:59 +0800

    add complex enum test case

---
+++ src/main/java/com/power/doc/enums/ComplexEnum.java
@@ -0,0 +1,57 @@
+package com.power.doc.enums;
+
+import java.util.function.BiFunction;
+
+/**
+ * 复杂类型的枚举，用于测试qdox
+ */
+public enum ComplexEnum {
+
+    ONE((((((System.currentTimeMillis() < 0))))) ? (BiFunction<String, String, String>) (a, b) -> {
+        String s = ("1)}}}}}}");
+        int i = ((((2))));
+        return a;
+    } : null),
+    ONE3((BiFunction<String, String, String>) (a, b) -> a + b + "\"\\)))}}}}"),
+    //
+    ONE2((BiFunction<String, String, String>) String::concat),
+    //
+    ONE4(null, null, (BiFunction<String, String, String>) (a, b) -> {
+        String s = ("1)}}}}}}");
+        int i = ((((2))));
+        return a;
+    }, (BiFunction<String, String, String>) (a, b) -> a + b + "\"\\)))}}}}", 1),
+
+    SUCCESS {
+        String name = "123";
+    }
+
+
+
+    ;
+
+    ComplexEnum() {
+    }
+
+    ComplexEnum(BiFunction<String, String, String> f) {
+        int i = (((((1)))));
+    }
+
+    ComplexEnum(BiFunction<String, String, String> f1,
+             BiFunction<String, String, String> f2,
+             BiFunction<String, String, String> f3,
+             BiFunction<String, String, String> f4,
+             int i) {
+        Runnable f = this::f2;
+    }
+
+
+    void f1() {
+
+    }
+
+    BiFunction<String, String, String> f2() {
+        return null;
+    }
+
+}
