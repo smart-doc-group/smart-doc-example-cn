71e4b59d5f424e91202e43b3fcf22a2bc8bb31c7
chenqi <1535985458@qq.com>
2022-05-29 11:06:10 +0800

    test(controller): test double type

--- src/main/java/com/power/doc/entity/Car.java
+++ src/main/java/com/power/doc/entity/Car.java
@@ -1,11 +1,19 @@
 package com.power.doc.entity;
 
+import lombok.Data;
+
 /**
  * @author yu 2020/8/25.
  */
+@Data
 public class Car {
     /**
      * 车名称
      */
     private String name;
+
+    /**
+     * 长度
+     */
+    private Double length;
 }
