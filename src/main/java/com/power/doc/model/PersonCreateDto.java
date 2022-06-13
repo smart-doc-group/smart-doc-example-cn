2f8b040bb9cd77c2853315011d96c13ab5bf2723
shalousun <smartdoc647@gmail.com>
2022-06-13 21:46:34 +0800

    升级编译级别

--- src/main/java/com/power/doc/model/PersonCreateDto.java
+++ src/main/java/com/power/doc/model/PersonCreateDto.java
@@ -2,12 +2,17 @@ package com.power.doc.model;
 
 import com.power.doc.enums.Sex;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * @author yu 2021/7/11.
  */
 public class PersonCreateDto {
+    /**
+     * 谈钱不伤感情
+     */
+    private BigDecimal money;
 
     /**
      * 名字
@@ -25,4 +30,6 @@ public class PersonCreateDto {
      * 子女ID
      */
     private List<Integer> childrenIds;
+
+
 }
