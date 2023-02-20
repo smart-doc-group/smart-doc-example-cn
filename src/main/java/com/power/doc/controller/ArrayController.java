3aac839cda6205d5b90a84ab61356fb75ad3028a
yusun <836575280@qq.com>
2023-02-20 19:15:25 +0800

    add: https://github.com/smart-doc-group/smart-doc/issues/405 测试用例

---
+++ src/main/java/com/power/doc/controller/ArrayController.java
@@ -0,0 +1,35 @@
+package com.power.doc.controller;
+
+import com.power.common.model.CommonResult;
+import com.power.doc.entity.SimpleUser;
+import com.power.doc.model.Subject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 测试Array
+ */
+@RestController
+public class ArrayController {
+
+    /**
+     * Array test
+     * @return
+     */
+    @PostMapping("arr")
+    public CommonResult<Subject[]> testArray() {
+        return CommonResult.ok();
+    }
+
+    /**
+     * Array test1
+     * @return
+     */
+    @PostMapping("arr1")
+    public CommonResult<List<Subject>> testArray1() {
+        return CommonResult.ok();
+    }
+
+}
