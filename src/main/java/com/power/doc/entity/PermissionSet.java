2b8f6bbf7ec82d4c071c82f82a967d93e36ea520
shalousun <836575280@qq.com>
2023-05-15 10:43:12 +0800

    update: 升级smart-doc到2.6.9

--- src/main/java/com/power/doc/entity/PermissionSet.java
+++ src/main/java/com/power/doc/entity/PermissionSet.java
@@ -1,5 +1,6 @@
 package com.power.doc.entity;
 
+import com.power.common.enums.HttpCodeEnum;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,10 +11,17 @@ import java.util.Set;
 @Builder
 public class PermissionSet {
 
+    /**
+     * 来自common-util的枚举
+     */
+    private HttpCodeEnum codeEnum;
+
     private int id;
 
     /**
      * 权限，jdk 17关键字
      */
     private Set<Permit> permits;
+
+
 }
