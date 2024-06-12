8819c27f2a384c25d18ee79771bda6303e1a7231
shalousun <836575280@qq.com>
2024-06-12 09:54:01 +0800

    feat: add usage test

---
+++ src/main/java/com/power/doc/model/circular/TreeNode.java
@@ -0,0 +1,20 @@
+package com.power.doc.model.circular;
+
+import java.util.List;
+
+public class TreeNode<T> {
+
+    /**
+     * 子节点
+     */
+    private List<TreeNode<T>> children;
+
+    /**
+     * 节点值
+     */
+    private T data;
+
+
+
+
+}
