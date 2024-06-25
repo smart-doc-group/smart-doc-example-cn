8b3bdc5edcafea58ddf8f907f91be86f8e6d58ca
linwumingshi <linwumingshi@gmail.com>
2024-06-25 22:08:06 +0800

    feat: Add example of @JsonFormat on TimeClass、NumberClass、Enum

--- src/main/java/com/power/doc/controller/json/SerializerTestController.java
+++ src/main/java/com/power/doc/controller/json/SerializerTestController.java
@@ -1,14 +1,15 @@
 package com.power.doc.controller.json;
 
+import com.power.common.model.CommonResult;
 import com.power.doc.model.SerializerTestModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * Jackson ToStringSerializer序列化测试
+ * Jackson ToStringSerializer test
+ *
  * @author yusun 2024/01/30
+ * @see <a href="https://github.com/TongchengOpenSource/smart-doc/issues/833">issues #833</a>
+ * @see <a href="https://github.com/TongchengOpenSource/smart-doc/issues/687">issues #687</a>
  */
 @RestController
 @RequestMapping("/serializer")
@@ -17,10 +18,11 @@ public class SerializerTestController {
 
     /**
      * 测试方法
-     * @param model
+     *
+     * @param model 测试参数
      */
-    @GetMapping
-    public void testSerializer(@RequestBody SerializerTestModel model) {
-
+    @PostMapping
+    public CommonResult<SerializerTestModel> testSerializer(@RequestBody SerializerTestModel model) {
+        return CommonResult.ok().setResult(model);
     }
 }
