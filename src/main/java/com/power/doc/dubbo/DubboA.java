b870876d6dceacb31de1a7d98ddfe6c9f83dbaf2
linwumingshi <linwumingshi@gmail.com>
2024-08-03 17:32:11 +0800

    feat: :sparkles:  Add `javadoc` examples && Add override parent class or interface method

--- src/main/java/com/power/doc/dubbo/DubboA.java
+++ src/main/java/com/power/doc/dubbo/DubboA.java
@@ -2,25 +2,38 @@ package com.power.doc.dubbo;
 
 import java.util.List;
 
-public interface DubboA<T>{
+public interface DubboA<T> {
 
     /**
-     * Common接口
+     * 1Common接口 DubboA的1
+     *
      * @param param
      * @return
      */
     Integer common(T param);
 
     /**
-     * Common接口2
+     * 2Common接口2 DubboA的2
+     *
      * @param result
      */
     void common2(List<T> result);
 
     /**
-     * Common接口2
+     * 3Common接口2 DubboA的3
+     *
      * @param result
      * @return
      */
     T common3(List<T> result);
+
+    /**
+     * 4默认新增方法
+     *
+     * @param entity T
+     * @return T
+     */
+    default T save(T entity) {
+        return entity;
+    }
 }
---
+++ src/main/java/com/power/doc/dubbo/impl/DubboInterfaceImplExtend.java
@@ -0,0 +1,29 @@
+package com.power.doc.dubbo.impl;
+
+import com.power.doc.entity.Order;
+import org.apache.dubbo.config.annotation.DubboService;
+
+/**
+ * DubboInterfaceImpl2继承 DubboInterfaceImpl
+ *
+ * @author yusun
+ * @version 2.0.0
+ * @dubbo
+ * @service test
+ * @protocol dubbo
+ */
+@DubboService(interfaceName = "testExtend", protocol = "grpc", version = "2.0.0")
+public class DubboInterfaceImplExtend extends DubboInterfaceImpl {
+
+    /**
+     * 13测试方法testConstantsRequestParam333 重写 order2222 DubboInterfaceImpl2 的
+     *
+     * @param order2222 order2222
+     * @param aa        aa
+     * @return dfd
+     */
+    @Override
+    public Order testConstantsRequestParam333(Order order2222, String aa) {
+        return new Order();
+    }
+}
