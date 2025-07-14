3509ab7f7eed5dea5877505e88188750b888ecf7
shalousun <836575280@qq.com>
2025-07-14 21:50:29 +0800

    feat: 添加测试用例

---
+++ src/main/java/com/power/doc/service/Something.java
@@ -0,0 +1,28 @@
+package com.power.doc.service;
+
+public interface Something {
+    default String processQuery(String param) {
+        // Complex method with text block
+        String query = """
+           SELECT id, name, email 
+           FROM users 
+           WHERE name = '"" + param + ""' 
+           AND active = true""";
+
+        // Other processing logic
+        return query.toUpperCase();
+    }
+
+    default String getBlockOfHtml() {
+        return """
+            <html>
+
+                <body>
+                    <span>example text</span>
+                </body>
+            </html>""";
+    }
+
+
+}
+
